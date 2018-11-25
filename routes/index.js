9e49159de391ce94b42be15a5b36d3a66c5439b0
mahui <1293777900@qq.com>
2018-11-25 12:15:55 +0800

    项目静态页面基本完成

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',function (req,res){
+  res.render('registar');
+})
+router.get('/login', function (req, res) {
+  res.render('login');
+})
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/user.html', function (req, res) {
